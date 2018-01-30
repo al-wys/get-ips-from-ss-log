840a438b49a6027e3d031a74ce78959e1b7e7806
Frank Wang <yiswang@microsoft.com>
2018-01-30 16:49:04 +0800

    Initial project

---
+++ GetIPsFromSSLog/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace GetIPsFromSSLog
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ofdSsLog = new System.Windows.Forms.OpenFileDialog();
+            this.btnChoseFile = new System.Windows.Forms.Button();
+            this.txtIPs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ofdSsLog
+            // 
+            this.ofdSsLog.FileName = "SS Log";
+            this.ofdSsLog.Filter = "Log files (*.log)|*.log";
+            this.ofdSsLog.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdSsLog_FileOk);
+            // 
+            // btnChoseFile
+            // 
+            this.btnChoseFile.Location = new System.Drawing.Point(61, 46);
+            this.btnChoseFile.Name = "btnChoseFile";
+            this.btnChoseFile.Size = new System.Drawing.Size(108, 45);
+            this.btnChoseFile.TabIndex = 0;
+            this.btnChoseFile.Text = "Chose File";
+            this.btnChoseFile.UseVisualStyleBackColor = true;
+            this.btnChoseFile.Click += new System.EventHandler(this.btnChoseFile_Click);
+            // 
+            // txtIPs
+            // 
+            this.txtIPs.Location = new System.Drawing.Point(25, 115);
+            this.txtIPs.Multiline = true;
+            this.txtIPs.Name = "txtIPs";
+            this.txtIPs.ReadOnly = true;
+            this.txtIPs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtIPs.Size = new System.Drawing.Size(498, 315);
+            this.txtIPs.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 460);
+            this.Controls.Add(this.txtIPs);
+            this.Controls.Add(this.btnChoseFile);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Get IPs from SS log";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofdSsLog;
+        private System.Windows.Forms.Button btnChoseFile;
+        private System.Windows.Forms.TextBox txtIPs;
+    }
+}
+
---
+++ GetIPsFromSSLog/MainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GetIPsFromSSLog
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChoseFile_Click(object sender, EventArgs e)
+        {
+            ofdSsLog.ShowDialog();
+        }
+
+        private void ofdSsLog_FileOk(object sender, CancelEventArgs e)
+        {
+            if (ofdSsLog.CheckFileExists)
+            {
+                var ipSet = new HashSet<string>();
+                var ipInfoBuilder = new StringBuilder();
+
+                var lineNum = 0;
+
+                using (var logFileStream = ofdSsLog.OpenFile())
+                {
+                    using (var reader = new StreamReader(logFileStream))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            // sample: 2018-01-29 13:32:10 INFO     connecting x.x.x.x:xxxx from x.x.x.x:xxxx
+                            var lineText = reader.ReadLine();
+                            var startIndex = lineText.IndexOf("from ");
+
+                            if (startIndex > -1)
+                            {
+                                startIndex += 5;
+                                var endIndex = lineText.LastIndexOf(':');
+
+                                var ip = lineText.Substring(startIndex, endIndex - startIndex);
+
+                                // Append this IP if it wasn't contained in the list
+                                if (!ipSet.Contains(ip))
+                                {
+                                    ipSet.Add(ip);
+                                    ipInfoBuilder.AppendLine(ip);
+                                }
+                            }
+
+                            lineNum++;
+                        }
+                    }
+                }
+
+                // Get the line number in the information
+                ipInfoBuilder.AppendLine();
+                ipInfoBuilder.AppendFormat("Total line number: {0}", lineNum);
+
+                txtIPs.Text = ipInfoBuilder.ToString();
+            }
+        }
+    }
+}
